22c7ec999f8febbd9f8d8fd7efb3c04d5b405ee0
Nicholas Turbide <nturb007@uottawa.ca>
2020-09-22 20:22:25 -0400

    I am, once again, committing to see if this works

--- Python tings.py
+++ Python tings.py
@@ -1,3 +1,5 @@
 print("This is kinda crazy, if it works of course.")
 
 print("Learning how to use git hub is weird...")
+
+print("You are able to stage and commit directly from MS Studio Code, the features are useful. However, I still need to pass it by the github desktop app for some reason...")
