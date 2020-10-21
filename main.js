0d418c4a081860b2a13c45a68167efd2e206f1a5
Christina Song <christina.song202@gmail.com>
2020-10-21 14:55:41 -0400

    making function to choose random testimonials to display, committing before my laptop crashes

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
 /* CUSTOMER TESTIMONIALS */
 function generateRandomNums() {
-    var reviewLinks = []
+    var reviewLinks = ["./images/tweet1.png","./images/tweet2.png","./images/tweet3.png","./images/tweet4.png","./images/tweet5.png","./images/tweet6.png"]
     
-    review1 = Math.random(1,6);
-    review2 = Math.random(1,6);
-    review3 = Math.random(1,6);
-    console.log(review1, review2, review3);
-
-    if (review1 == 1) {
-        pass
-    }
-    else if (review1 == 2) {
-
-    }
-    else if (review1 == 3) {
-        
-    }
-    else if (review1 == 4) {
-        
-    }
-    else if (review1 == 5) {
-        
-    }
-    else {
-        
+    review1 = Math.random(0,5);
+    review2 = Math.random(0,5);
+    review3 = Math.random(0,5);
+    
+    // ensure that no 2 columns show the same testimonial
+    while ((review1==review2) || (review1==review3) || (review2==review3)) {
+        review1 = Math.random(0,5);
+        review2 = Math.random(0,5);
+        review3 = Math.random(0,5);
     }
 
+    document.getElementById("custReview1").src=reviewLinks[review1];
+    document.getElementById("custReview2").src=reviewLinks[review2];
+    document.getElementById("custReview3").src=reviewLinks[review3];
 }
