9711794fb50987a6660cc419c8d653fe7948472f
Christina Song <christina.song202@gmail.com>
2020-10-20 20:18:55 -0400

    added stuff to generateRandomNums function

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 /* CUSTOMER TESTIMONIALS */
 function generateRandomNums() {
+    var reviewLinks = []
+    
     review1 = Math.random(1,6);
     review2 = Math.random(1,6);
     review3 = Math.random(1,6);
     console.log(review1, review2, review3);
+
+    if (review1 == 1) {
+        pass
+    }
+    else if (review1 == 2) {
+
+    }
+    else if (review1 == 3) {
+        
+    }
+    else if (review1 == 4) {
+        
+    }
+    else if (review1 == 5) {
+        
+    }
+    else {
+        
+    }
+
 }
