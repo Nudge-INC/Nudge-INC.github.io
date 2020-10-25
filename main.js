b2f29a43f4089bb6348ac34d6eaf082046b5bc8c
Christina Song <christina.song202@gmail.com>
2020-10-24 20:57:07 -0400

    trying different ways to call function on page reload

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 /* CUSTOMER TESTIMONIALS */
-function generateRandomNums() {
-    var reviewLinks = ["./images/tweet1.png","./images/tweet2.png","./images/tweet3.png","./images/tweet4.png","./images/tweet5.png","./images/tweet6.png"]
+function displayReviews() {
+    // window.location.hash = "reload";
+    // window.location.reload();
+    alert("displayReviews() function")
     
-    review1 = Math.random(0,5);
-    review2 = Math.random(0,5);
-    review3 = Math.random(0,5);
+    var reviewLinks = ["./images/tweet1.png",
+    "./images/tweet2.png",
+    "./images/tweet3.png",
+    "./images/tweet4.png",
+    "./images/tweet5.png",
+    "./images/tweet6.png"]
+    
+    review1 = Math.round(Math.random(0,5));  // returns a random integer from 0 to 9
+    review2 = Math.round(Math.random(0,5));
+    review3 = Math.round(Math.random(0,5));
     
     // ensure that no 2 columns show the same testimonial
     while ((review1==review2) || (review1==review3) || (review2==review3)) {
-        review1 = Math.random(0,5);
-        review2 = Math.random(0,5);
-        review3 = Math.random(0,5);
+        review1 = Math.round(Math.random(0,5));  // returns a random integer from 0 to 9
+        review2 = Math.round(Math.random(0,5));
+        review3 = Math.round(Math.random(0,5));
     }
 
     document.getElementById("custReview1").src=reviewLinks[review1];
     document.getElementById("custReview2").src=reviewLinks[review2];
     document.getElementById("custReview3").src=reviewLinks[review3];
+    document.getElementById("testing").innerHTML=review1 + ", " + review2 + ", " + revew3;
 }
