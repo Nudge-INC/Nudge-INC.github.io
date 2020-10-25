1bb03d957ad8e067908c26bb18d327765a827d87
Christina Song <christina.song202@gmail.com>
2020-10-24 23:32:28 -0400

    finally got function to display random reviews when page is reloaded to workgit status

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function displayReviews() {
     // window.location.hash = "reload";
     // window.location.reload();
-    alert("displayReviews() function")
+    // alert("displayReviews() function");
     
     var reviewLinks = ["./images/tweet1.png",
     "./images/tweet2.png",
@@ -11,19 +11,21 @@ function displayReviews() {
     "./images/tweet5.png",
     "./images/tweet6.png"]
     
-    review1 = Math.round(Math.random(0,5));  // returns a random integer from 0 to 9
-    review2 = Math.round(Math.random(0,5));
-    review3 = Math.round(Math.random(0,5));
+    review1 = Math.floor(Math.random() * 6);  // returns a random integer from 0 to 5
+    review2 = Math.floor(Math.random() * 6);
+    review3 = Math.floor(Math.random() * 6);
+    // alert("generated random numbers");
     
     // ensure that no 2 columns show the same testimonial
     while ((review1==review2) || (review1==review3) || (review2==review3)) {
-        review1 = Math.round(Math.random(0,5));  // returns a random integer from 0 to 9
-        review2 = Math.round(Math.random(0,5));
-        review3 = Math.round(Math.random(0,5));
+        review1 = Math.floor(Math.random() * 6);  // returns a random integer from 0 to 5
+        review2 = Math.floor(Math.random() * 6);
+        review3 = Math.floor(Math.random() * 6);
+        // alert(review1 + ", " + review2 + ", " + review3)
     }
+    // alert("while loop finished");
 
     document.getElementById("custReview1").src=reviewLinks[review1];
     document.getElementById("custReview2").src=reviewLinks[review2];
     document.getElementById("custReview3").src=reviewLinks[review3];
-    document.getElementById("testing").innerHTML=review1 + ", " + review2 + ", " + revew3;
 }
