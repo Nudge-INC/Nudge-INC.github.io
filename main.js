2c6d41a1907dfbe5926c2bcb119d98a63d4d2062
Christina Song <christina.song202@gmail.com>
2020-10-27 12:17:15 -0400

    adding an animation

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 /* CUSTOMER TESTIMONIALS */
+
+// get scroll position of .upper-container
+// const sc = document.querySelector('.upper-container')
+// document.getElementById("scPos").innerHTML="scroll position: " + sc.scrollLeft + ", " + sc.scrollTop;
+function scrollPosition() {
+    var elmnt = document.getElementById("upper-container")
+    // var a = elmnt.scrollTop;
+    // var b = elmnt.scrollHeight - elmnt.clientHeight;
+    // var c = a / b;
+    // document.getElementById("scPos").innerHTML = a + " " + b + " " + c;
+    var y = elmnt.scrollTop;
+    var c = elment.clientHeight; // viewable height of upper-container in px
+    var anim_played = false;
+    var ite_count = 1;
+    //  && (document.getElementById("upper-container").style.left > 0)
+
+    if ((c >= 40) && (anim_played == false)) {
+        ite_count += 1;
+        alert("ite_count = " + ite_count);
+        document.getElementById("upper-container").style.animationPlayState = running;
+        anim_played = true;
+    }
+    else {
+        // document.getElementById("upper-container").style.animationPlayState = initial;
+        alert("upper-container is not visible")
+        document.getElementById("upper-container").style.animationPlayState = paused;
+        anim_played = false;
+    }
+}
+
+
+// window.addEventListener("scroll", (event) => {
+//     let scroll = this.scrollY;
+//     console.log(scroll)
+//     document.getElementById("scPos").innerHTML=scroll;
+// });
+
+// function to display random reviews every time the page is refreshed
 function displayReviews() {
 
     var reviewLinks = ["./images/tweet1.png",
