4796df15d9e4af50321f19b1a34d57f10935e07b
Christina Song <christina.song202@gmail.com>
2020-10-29 01:24:45 -0400

    cleaned up code

--- main.js
+++ main.js
@@ -1,37 +1,4 @@
 /* CUSTOMER TESTIMONIALS */
-// console.log("hello world");
-// var anim_played;
-
-// window.onscroll = function() {scrollPosition()};
-// function scrollPosition() {
-//     var elmnt = document.getElementById("upper-cont");
-//     var c = elmnt.clientHeight; // viewable height of upper-container in px
-//     var ite_count = Number(elmnt.style.animationIterationCount);
-
-//     if ((c >= 40) && (anim_played != true)) {
-//         ite_count += 1;
-//         console.log("anim_played = " + anim_played + "\nite_count = " + ite_count);    
-//         document.getElementById("upper-cont").style.animationIterationCount = ite_count;
-//         // document.getElementById("upper-cont").style.animationPlayState = "running";
-//         anim_played = true;
-//         console.log("anim_played = " + anim_played);
-//         // document.getElementById("upper-cont").style.animationIterationCount = 1;
-//     }
-//     else {
-//         document.getElementById("upper-cont").style.animationPlayState = "paused";
-//         anim_played = false;
-//         console.log("upper_container is not visible, anim_played = " + anim_played);
-//     }
-// }
-
-// window.addEventListener(
-//     "scroll",
-//     () => {
-//         // document.body.style.setProperty("--scroll", window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-//         console.log("scroll = " + --scroll);
-//     },
-//     false
-// );
 
 // function to display random reviews every time the page is refreshed
 function displayReviews() {
