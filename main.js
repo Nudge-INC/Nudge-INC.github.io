4271527b4135ae3dcea1b7d6b9b1a51e29888abb
Christina Song <christina.song202@gmail.com>
2020-10-29 15:27:02 -0400

    changed z-index of nav bar to 2

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 /* CUSTOMER TESTIMONIALS */
+// window.onload = setTimeout(shiftReviews, 12000);
 
 // function to display random reviews every time the page is refreshed
 function displayReviews() {
+
+    /*
+    TO DO
+    - figure out how to shuffle order of images
+    */
 
     var reviewLinks = ["./images/tweet1.png",
     "./images/tweet2.png",
@@ -9,20 +15,36 @@ function displayReviews() {
     "./images/tweet4.png",
     "./images/tweet5.png",
     "./images/tweet6.png",
-    "./images/tweet7.png"]
+    "./images/tweet7.png"];
+
+    var reviewOrder = ["","","","","","",""];
     
     review1 = Math.floor(Math.random() * reviewLinks.length);  // returns a random integer from 0 to array length - 1
     review2 = Math.floor(Math.random() * reviewLinks.length);
     review3 = Math.floor(Math.random() * reviewLinks.length);
-    
-    // ensure that no 2 columns show the same testimonial
+
+    // // ensure that no 2 columns show the same testimonial
     while ((review1==review2) || (review1==review3) || (review2==review3)) {
         review1 = Math.floor(Math.random() * 6);
         review2 = Math.floor(Math.random() * 6);
         review3 = Math.floor(Math.random() * 6);
     }
+
+    // generate random review to display testimonials in
+    // for (i=0; i < displayReviews.length; i++) {
+    //     reviewOrder[i] = Math.floor(Math.random() * reviewLinks.length);
+    // }
+    
     // change src of images
     document.getElementById("custReview1").src=reviewLinks[review1];
     document.getElementById("custReview2").src=reviewLinks[review2];
     document.getElementById("custReview3").src=reviewLinks[review3];
+    // document.getElementById("custReview1").src=reviewOrder[0];
+    // document.getElementById("custReview2").src=reviewOrder[1];
+    // document.getElementById("custReview2").src=reviewOrder[2];
+}
+
+// function to shift reviews 
+function shiftReviews() {
+    // 
 }
