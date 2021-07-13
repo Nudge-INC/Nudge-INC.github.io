0622fd7a83b36e4f328cffbb7e5b8ce68fa15f08
Nicholas Turbide <nturb007@uottawa.ca>
2021-07-12 22:26:46 -0400

    update main.js

--- main.js
+++ main.js
@@ -4,180 +4,100 @@
 // function that fades the home page (opacity) as the page is scrolled down
 
 $(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
+  var scrollTop = $(this).scrollTop();
 
-    $('.home-background').css({
-        opacity: function() {
-            var elementHeight = $(this).height();
-            opacity = ((elementHeight - scrollTop - 100) / elementHeight);
-            return opacity;
-        }
-    });
+  $('.home-background').css({
+      opacity: function() {
+          var elementHeight = $(this).height();
+          opacity = ((elementHeight - scrollTop - 100) / elementHeight);
+          return opacity;
+      }
+  });
 });
 
-$(".home-text").delay(1500).animate({"opacity": "1"}, 1000);
+$(".home-text").delay(2000).animate({"opacity": "1"}, 1000);
 
-$(".arrow").delay(2500).animate({"opacity": "1"}, 1000);
+$(".arrow").delay(3000).animate({"opacity": "1"}, 1000);
 
-// main
-/* ------------CUSTOMER TESTIMONIALS------------------- */
-// declare global variables
-var reviewLinks = ["./images/tweet1.png",
-    "./images/tweet2.png",
-    "./images/tweet3.png",
-    "./images/tweet4.png",
-    "./images/tweet5.png",
-    "./images/tweet6.png"];
-var shift = 0;
-window.setInterval(shiftReviews, 7500);
 
-// function to shift reviews 
-function shiftReviews() {
-    temp = "";
-    firstImage = reviewLinks[0];
-    for (i = 0; i < reviewLinks.length - 1; i++) {
-        reviewLinks[i] = reviewLinks[i+1];
+// CUSTOMER TESTIMONIALS
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
     }
-    shift += 1;
-    if (shift > 5) {
-        shift -= 6;
-    }
-    console.log("shift = " + shift);
-    reviewLinks[reviewLinks.length-1] = firstImage;
-    displayReviews();
-}
-
-// function to display reviews
-function displayReviews() {
-
-    // set photos
-    for (i = 1; i < 4; i++) {
-        document.getElementById("custReview" + i).src=reviewLinks[i-1];
-    }
-
-    // set dots
-    for (i = 1; i < reviewLinks.length+1; i++) {
-        document.getElementById("dot" + i).style.color = "rgba(255,255,255,0.3)";
-    }
-
-    document.getElementById("dot" + (shift + 1)).style.color = "rgba(255,255,255,0.6)";
-}
-
-function dot1() {
-    console.log("dot1 clicked");
-    reviewLinks = ["./images/tweet1.png",
-    "./images/tweet2.png",
-    "./images/tweet3.png",
-    "./images/tweet4.png",
-    "./images/tweet5.png",
-    "./images/tweet6.png"];
-    shift = 0;
-    displayReviews();
-}
-function dot2() {
-    console.log("dot2 clicked");
-    reviewLinks = ["./images/tweet2.png",
-    "./images/tweet3.png",
-    "./images/tweet4.png",
-    "./images/tweet5.png",
-    "./images/tweet6.png",
-    "./images/tweet1.png"];
-    shift = 1;
-    displayReviews();
-}
-function dot3() {
-    console.log("dot3 clicked");
-    reviewLinks = ["./images/tweet3.png",
-    "./images/tweet4.png",
-    "./images/tweet5.png",
-    "./images/tweet6.png",
-    "./images/tweet1.png",
-    "./images/tweet2.png"];
-    shift = 2;
-    displayReviews();
-}
-function dot4() {
-    console.log("dot4 clicked");
-    reviewLinks = ["./images/tweet4.png",
-    "./images/tweet5.png",
-    "./images/tweet6.png",
-    "./images/tweet1.png",
-    "./images/tweet2.png",
-    "./images/tweet3.png"];
-    shift = 3;
-    displayReviews();
-}
-function dot5() {
-    console.log("dot5 clicked");
-    reviewLinks = ["./images/tweet5.png",
-    "./images/tweet6.png",
-    "./images/tweet1.png",
-    "./images/tweet2.png",
-    "./images/tweet3.png",
-    "./images/tweet4.png"];
-    shift = 4;
-    displayReviews();
-}
-function dot6() {
-    console.log("dot6 clicked");
-    reviewLinks = ["./images/tweet6.png",
-    "./images/tweet1.png",
-    "./images/tweet2.png",
-    "./images/tweet3.png",
-    "./images/tweet4.png",
-    "./images/tweet5.png"];
-    shift = 5;
-    displayReviews();
-}
+  }
+});
 
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
 function myFunction2() {
-    var dots = document.getElementById("dots2");
-    var moreText = document.getElementById("more2");
-    var btnText = document.getElementById("myBtn2");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
 function myFunction3() {
-    var dots = document.getElementById("dots3");
-    var moreText = document.getElementById("more3");
-    var btnText = document.getElementById("myBtn3");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
 
 // Contact Us
 function ThankYou(){
-    var contactbutton = document.getElementById("SupportPopup");
-    contactbutton.classList.toggle("show");
+  var contactbutton = document.getElementById("SupportPopup");
+  contactbutton.style.display = "inline-block";
+  contactbutton.classList.toggle("show");
+  setTimeout(hideThankYou, 4000);
+}
+function hideThankYou(){
+  var contactbutton = document.getElementById("SupportPopup");  
+  contactbutton.style.display = "none";
+  contactbutton.classList.toggle("hide");
 }
